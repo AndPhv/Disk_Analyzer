bbfb69eb5f79948dd0301c940a635a159bc0c584
AndPhv <puhov2001@gmail.com>
2022-02-09 12:09:50 +0300

    Creating a variable Stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    private Stage stage;
+
     public static void main(String[] args)
     {
-        //launch(args);
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
+    public void start(Stage primaryStage) throws Exception
+    {
+        this.stage = stage;
+        stage.setTitle("Disk analyzer");
     }
 }
